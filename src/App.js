848a19b447e984093340428082e83760605d1e97
CO <2236562@cognizant.com>
2023-12-06 10:19:52 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 function App() {
+  const initialState = {
+    questions: [],
+    status: "loading",
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "dataReceived":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      case "dataFailed":
+        return {
+          ...state,
+          status: "error",
+        };
+      case "start":
+        return {
+          ...state,
+          status: "start",
+        };
+      default:
+        throw new Error("Action unknown");
+    }
+  };
+
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <DateCounter /> */}
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
+        {status === "error" && <Error />}
+      </Main>
     </div>
   );
 }
